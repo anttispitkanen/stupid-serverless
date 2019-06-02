d961e98b134fabeb4d53759c672565d2b7e2766e
Antti Pitkänen <anttis.pitkanen@gmail.com>
2019-06-02 20:13:42 +0300

    Initial commit, wikipedia API set up

---
+++ index.js
@@ -0,0 +1,25 @@
+const serverless = require('serverless-http');
+const express = require('express');
+const wikipediaClient = require('./clients/wikipedia');
+
+const app = express();
+
+app.get('/random-text', async (req, res) => {
+  try {
+    const lang = req.query && req.query.lang;
+
+    const randomTitle = await wikipediaClient.getRandomWikipediaArticle(lang);
+
+    const results = await wikipediaClient.fetchSentenceFromWikipedia(
+      lang,
+      randomTitle,
+    );
+
+    res.json(results);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err });
+  }
+});
+
+module.exports.handler = serverless(app);
